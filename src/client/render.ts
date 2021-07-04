8165d2049b5ead4aab73eb7481713e2a169834c7
Taras Novak <taras.novak@randomfractals.com>
2021-07-04 11:19:55 -0500

    switch to ObservableHQ Inputs table component for data table display (#12)

--- src/client/render.ts
+++ src/client/render.ts
@@ -1,10 +1,5 @@
-/**
- * Import CSS modules.
- * @see https://github.com/css-modules/css-modules
- * Configure it in webpack.config.js
- */
-import * as style from './style.css';
 import type {RendererContext} from 'vscode-notebook-renderer';
+const inputs = require('@observablehq/inputs');
 
 interface IRenderInfo {
   container: HTMLElement;
@@ -14,22 +9,9 @@ interface IRenderInfo {
 }
 
 export function render({container, mime, value}: IRenderInfo) {
-  const table: HTMLTableElement = document.createElement('table');
-  table.className = 'data-table';
-  let data = value.data ? value.data: value;
-  for(let i = 0; i < data.length; i++) {
-    let child = data[i];
-    if (i === 0 ) {
-      addHeaders(table, Object.keys(child));
-    }
-    let row = table.insertRow();
-    Object.keys(child).forEach(function(k) {
-      let cell = row.insertCell();
-      cell.appendChild(document.createTextNode(child[k]));
-    });
-  }
+  const data = value.data ? value.data: value;
+  const table = inputs.table(data);
   console.log(`data.table: mime-type=${mime}`);
-  // console.log(JSON.stringify(data, null, 2));
   container.appendChild(table);
 }
 
@@ -37,12 +19,4 @@ if (module.hot) {
   module.hot.addDisposeHandler(() => {
       // TODO: dispose resources and save renderer state
   });
-}
-
-function addHeaders(table: any, keys: string[]) {
-  const row = table.insertRow();
-  for(let i = 0; i < keys.length; i++ ) {
-    let cell = row.insertCell();
-    cell.appendChild(document.createTextNode(keys[i]));
-  }
 }
