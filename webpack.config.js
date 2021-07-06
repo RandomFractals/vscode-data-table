88192ec038b4703d2edb7647cdda268e1ddf6775
Taras Novak <taras.novak@randomfractals.com>
2021-07-06 07:53:41 -0500

    move renderer code to src/renderer (#21)

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const { DefinePlugin } = require('webpack');
 const path = require('path');
@@ -8,9 +9,9 @@ const devServerPort = 8111;
 module.exports = (env, argv) => ({
   mode: argv.mode,
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
-  entry: './src/client/index.ts',
+  entry: './src/renderer/index.ts',
   output: {
-    path: path.join(__dirname, 'out', 'client'),
+    path: path.join(__dirname, 'out', 'renderer'),
     filename: outputFilename,
     publicPath: '',
     libraryTarget: 'module',
@@ -23,21 +24,21 @@ module.exports = (env, argv) => ({
   },
   module: {
     rules: [
-      // Allow importing ts(x) files:
+      // allow importing ts(x) files:
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         options: {
-          configFile: 'src/client/tsconfig.json',
+          configFile: 'src/renderer/tsconfig.json',
           // transpileOnly enables hot-module-replacement
           transpileOnly: true,
           compilerOptions: {
-            // Overwrite the noEmit from the client's tsconfig
+            // overwrite the noEmit renderers tsconfig
             noEmit: false,
           },
         },
       },
-      // Allow importing CSS modules:
+      // allow importing CSS modules
       {
         test: /\.css$/,
         use: [
@@ -56,8 +57,8 @@ module.exports = (env, argv) => ({
   devServer: {
     port: devServerPort,
     hot: true,
-    // Disable the host check, otherwise the bundle running in VS Code won't be
-    // able to connect to the dev server
+    // disable host check, otherwise the bundle running in vscode 
+    // won't be able to connect to the dev server
     disableHostCheck: true,
     writeToDisk: true,
     headers: { 'Access-Control-Allow-Origin': '*' },
@@ -65,11 +66,11 @@ module.exports = (env, argv) => ({
   plugins: [
     new ForkTsCheckerWebpackPlugin({
       typescript: {
-        tsconfig: 'src/client/tsconfig.json',
+        tsconfig: 'src/renderer/tsconfig.json',
       },
     }),
     new DefinePlugin({
-      // Path from the output filename to the output directory
+      // path from the output filename to the output directory
       __webpack_relative_entrypoint_to_root__: JSON.stringify(
         path.posix.relative(path.posix.dirname(`/${outputFilename}`), '/'),
       ),
