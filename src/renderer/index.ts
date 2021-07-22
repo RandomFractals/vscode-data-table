b9fc71d7c349528de72d3c30a0d98f4f0b9a2b97
Taras Novak <taras.novak@randomfractals.com>
2021-07-22 09:00:57 -0500

    got rid of renderer output shadow root to fix data table display (#46 and #47)

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -17,18 +17,9 @@ __webpack_public_path__ = new URL(scriptUrl.replace(/[^/]+$/, '') +
 export const activate: ActivationFunction = context => {
   return {
     renderOutputItem(outputItem: OutputItem, element: HTMLElement) {
-      let shadow = element.shadowRoot;
-      if (!shadow) {
-        shadow = element.attachShadow({mode: 'open'});
-        const root = document.createElement('div');
-        root.id = 'root';
-        shadow.append(root);
-      }
-      const root = shadow.querySelector<HTMLElement>('#root')!;
-      errorOverlay.wrap(root, () => {
-        root.innerHTML = '';
+      errorOverlay.wrap(element, () => {
         const cellOutputContainer: HTMLDivElement = document.createElement('div');
-        root.appendChild(cellOutputContainer);
+        element.appendChild(cellOutputContainer);
         render({
           container: cellOutputContainer,
           mimeType: outputItem.mime,
