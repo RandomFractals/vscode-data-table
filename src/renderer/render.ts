6fe3d521bb6741f9614c4abe7d580eecd0c42239
Taras Novak <taras.novak@randomfractals.com>
2021-07-25 07:49:48 -0500

    enable dynamic table row creation for faster data loading (#62)

--- src/renderer/render.ts
+++ src/renderer/render.ts
@@ -1,10 +1,12 @@
 import type {
-  OutputItem, RendererContext
+  OutputItem, 
+  RendererContext
 } from 'vscode-notebook-renderer';
 import { OutputLoader } from './outputLoader';
+import './style.css';
 const aq = require('arquero');
 const inputs = require('@observablehq/inputs');
-import './style.css';
+
 
 /**
  * Notebook cell output render info.
@@ -39,7 +41,6 @@ export function render(output: IRenderInfo) {
       layout: 'auto',
       width: 'auto',
       height: 360,
-      rows: lengthOf(data)
     });
 
     // add table to cell data output container
@@ -58,28 +59,4 @@ if (module.hot) {
   module.hot.addDisposeHandler(() => {
     // TODO: dispose resources and save renderer state
   });
-}
-
-/**
- * Gets data length.
- * @param data Data array, set, map, or arquero table.
- * @returns Dataset length/size or row count.
- */
- function lengthOf(data: any) {
-  if (typeof data.length === 'number') {
-    // array or array-like
-    return data.length;
-  }
-
-  if (typeof data.size === 'number') {
-    // map or set
-    return data.size;
-  }
-
-  if (typeof data.numRows === 'function') {
-    // arquero
-    return data.numRows();
-  }
-
-  return 0;
 }
