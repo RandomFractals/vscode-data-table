5c75e3905e9c5ebf9249562ada7c672f23b43bf3
Taras Novak <taras.novak@randomfractals.com>
2021-08-01 07:03:09 -0500

    add basic ext. constats.ts with notebook examples command (#75)

---
+++ src/extension/constants.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+
+// extension constants
+export const PublisherId: string = 'RandomFractalsInc';
+export const ExtensionName: string = 'vscode-data-table';
+export const ExtensionId: string = 'data.table';
+export const ExtensionDisplayName: string = 'Data Table';
+
+// command constants
+export const NotebookExamplesCommand: string = `${ExtensionId}.notebook.examples`;
