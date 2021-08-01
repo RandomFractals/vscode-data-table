be3c544c7f52fd0c1232773b6d02cf916bc56a1e
Taras Novak <taras.novak@randomfractals.com>
2021-08-01 06:43:26 -0500

    add built-in data table notebook examples to new ext. config.ts (#75)

---
+++ src/extension/config.ts
@@ -0,0 +1,43 @@
+// built-in data table notebook examples
+export const notebookExamples: Array<any> = [
+  {
+    name: 'Chicago Traffic Tracker',
+    file: './notebooks/chicago-traffic-tracker.restbook',
+    description: 'Current Chicago Traffic Tracker REST Book',
+  },
+  {
+    name: 'USA Airports',
+    file: './notebooks/usa-airports.ipynb',
+    description: 'USA Airports TypeScript Notebook',
+  },
+  {
+    name: 'Chicago Speed Cameras',
+    file: './notebooks/chicago-speed-cameras.ipynb',
+    description: 'Chicago Speed Cameras .NET Interactive Notebook',
+  },
+  {
+    name: 'Chicago Red Light Cameras',
+    file: './notebooks/chicago-red-light-cameras.ipynb',
+    description: 'Chicago Red Light Cameras Pyolite Notebook',
+  },
+  {
+    name: 'World Cities',
+    file: './notebooks/world-cities.restbook',
+    description: 'Major World Cities REST Book',
+  },
+  {
+    name: 'World Rivers',
+    file: './notebooks/world-rivers.restbook',
+    description: 'Major World Rivers REST Book',
+  },
+  {
+    name: 'World Lakes',
+    file: './notebooks/world-lakes.restbook',
+    description: 'Major World Lakes REST Book',
+  },
+  {
+    name: 'World GDP',
+    file: './notebooks/world-gdp.restbook',
+    description: 'World GDP REST Book',
+  },
+];
