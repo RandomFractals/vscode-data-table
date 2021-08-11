575b22097839d6bee8f4bd06039e3ef7a3151886
Taras Novak <taras.novak@randomfractals.com>
2021-08-11 12:57:31 -0500

    data loading and text display code cleanup (#94)

--- src/renderer/dataTableRenderer.ts
+++ src/renderer/dataTableRenderer.ts
@@ -23,17 +23,11 @@ interface IRenderInfo {
  * @param output Notebook cell output info to render.
  */
 export function render(output: IRenderInfo) {
-  let data: any;
-  const outputLoader: OutputLoader = new OutputLoader(output.value, output.mimeType);
   console.log(`data.table:mimeType: ${output.mimeType}`);
-  switch (output.mimeType) {
-    case 'application/json':
-    case 'text/csv':
-    case 'text/plain':
-    case 'application/vnd.code.notebook.stdout':
-      data = outputLoader.getData();
-      break;
-  }
+
+  // load and parse output data
+  const outputLoader: OutputLoader = new OutputLoader(output.value, output.mimeType);
+  let data: any = outputLoader.getData();
 
   if (Array.isArray(data)) {
     // create data table view
@@ -47,8 +41,20 @@ export function render(output: IRenderInfo) {
     output.container.appendChild(table);
   }
   else {
-    // output text in pre/code tags
-    output.container.appendChild(htl.html`<pre class="text-output"><code>${data}</code></pre>`);
+    // create text output display nodes
+    const pre = document.createElement('pre');
+    pre.className = 'text-output';
+    const code = document.createElement('code');
+    if (typeof data !== 'string') {
+      // stringify json data
+      code.textContent = JSON.stringify(data, null, 2);
+    }
+    else {
+      // show cell output text
+      code.textContent = output.value.text();
+    }
+    pre.appendChild(code);
+    output.container.appendChild(pre);
   }
 }
 
